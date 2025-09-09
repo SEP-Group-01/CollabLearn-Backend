fcb52a27678bacdc387b5373a24cd58b33d4eb0b
ThejithaR <146850506+ThejithaR@users.noreply.github.com>
2025-09-10 00:40:07 +0530

    kafka setup

--- python/document-query-service/app/kafka/producer.py
+++ python/document-query-service/app/kafka/producer.py
@@ -13,14 +13,14 @@ class KafkaProducerService:
     async def stop(self):
         await self.producer.stop()
 
-    async def send_message(self, topic: str, key: str, value: str):
+    async def send_message(self, topic: str, correlation_id: str, value: str):
         """Send a message to Kafka"""
         try:
             await self.producer.send_and_wait(
                 topic,
-                key=key.encode("utf-8"),
+                key=correlation_id.encode("utf-8"),
                 value=value.encode("utf-8"),
             )
-            print(f"[KafkaProducer] Sent message to {topic}: {value}")
+            print(f"[KafkaProducer] Sent message to {topic}: {value} using correlation ID: {correlation_id}")
         except Exception as e:
             print(f"[KafkaProducer] Error: {e}")
